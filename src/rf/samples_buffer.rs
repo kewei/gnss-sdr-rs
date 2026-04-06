3996bce168e9d17ab7c800b0c3d9b2e60d056832
Kewei Zhang <zkw20080810@gmail.com>
2026-04-06 14:28:04 +0200

    Update the sample buffer function to be type generic

--- src/rf/samples_buffer.rs
+++ src/rf/samples_buffer.rs
@@ -1,17 +1,18 @@
 use ringbuf::{traits::*, HeapRb, HeapProd, HeapCons};
 use num::complex::Complex;
 
-pub type Sample = Complex<f32>;
-pub static BUFFER_SIZE: usize = 16384;
+pub type SampleComplex = Complex<f32>;
+pub type SampleReal = f32;
+pub static BUFFER_SIZE: usize = 163840;
 
 
-pub struct SamplesRingBuffer {
-    pub producer: HeapProd<Sample>,
-    pub consumer: HeapCons<Sample>,
+pub struct SamplesRingBuffer<T> {
+    pub producer: HeapProd<T>,
+    pub consumer: HeapCons<T>,
 }
 
-pub fn create_samples_ring_buffer(size: usize) -> SamplesRingBuffer {
-    let rb = HeapRb::<Sample>::new(size);
+pub fn create_samples_ring_buffer<T>(size: usize) -> SamplesRingBuffer<T> {
+    let rb = HeapRb::<T>::new(size);
     let (mut producer, mut consumer) = rb.split();
     SamplesRingBuffer { producer, consumer }
 }
